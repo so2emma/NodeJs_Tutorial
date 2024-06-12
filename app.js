0be8dba18bd2e6186e420806b2d921793ccdc5b5
so2emma <osoemmanuel1969@gmail.com>
2024-06-12 18:02:56 +0100

    styling pages and rendering content statically

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const port = 3000;
 
@@ -11,11 +13,13 @@ const shopRoutes = require("./routes/shop");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "public")))
+
 app.use('/admin', adminRoutes);
 app.use('/shop', shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found</h1>")
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
 })
 
 app.listen(port, () => {
