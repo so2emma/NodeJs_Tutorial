39438daf92e5a47fcc840749393d37818a909fed
so2emma <osoemmanuel1969@gmail.com>
2024-06-12 15:51:48 +0100

    using react router and adding filters

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const http = require("http");
-const routes = require("./routes");
+const express = require('express');
+const port = 3000;
 
-const server = http.createServer(routes.handler);
+const app = express(); 
 
-server.listen(3000);
+// this is for importing routes from the admin router page.
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+// This is for accepting json requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page not found</h1>")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+});
