9ec1dd06516dbd7ae47dfc21a12f7d19d8a0213a
so2emma <osoemmanuel1969@gmail.com>
2024-06-15 16:02:29 +0100

    Setting up model, storing post in the database, static images and error handling. fetching a single post

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
-const path = require('path');
+const path = require("path");
+require('dotenv').config()
 
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 const port = 8080;
 
-const app = express(); 
+const app = express();
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
 
 app.use(express.json()); // this is for application json
 app.use(express.urlencoded({ extended: true }));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use((req, res, next)=> {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message: message});
 })
 
 
-
-app.use('/feed', feedRoutes);
-
-
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+mongoose
+  .connect(
+    `mongodb+srv://${username}:${password}@cluster0.nmziuhm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => console.log("Connected!"))
+  .catch(() => console.log("Connection failed"));
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`)
+  console.log(`Server is running at http://localhost:${port}`);
 });
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,33 +1,69 @@
+const { validationResult } = require("express-validator");
+
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This are some contents",
-        imageUrl: "images/duck.jpg",
-        creator: {
-          name: "Oso Emmanuel",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res
+        .status(200)
+        .json({ message: "Fetched posts successfully", posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPosts = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, data entered is incorrect");
+    error.statusCode = 422;
+    throw error;
+  }
   const title = req.body.title;
   const content = req.body.content;
-
-  //Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {name: "Emmanuel"},
-      createdAt: new Date()
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/duck.jpg",
+    creator: { name: "Emmanuel" },
   });
+
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "Post fetched", post: post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
