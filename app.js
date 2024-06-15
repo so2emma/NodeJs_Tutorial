bd28326ce9a38aa0fbe76442baea3149c8e4d76d
so2emma <osoemmanuel1969@gmail.com>
2024-06-15 22:13:33 +0100

    uploading images

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const path = require("path");
-require('dotenv').config()
-
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const port = 8080;
-
-const app = express();
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 
 const feedRoutes = require("./routes/feed");
 
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4() + "_" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  }else {
+    cb(null, false);
+  }
+};
+
 app.use(express.json()); // this is for application json
 app.use(express.urlencoded({ extended: true }));
+app.use(multer({storage: storage, fileFilter: fileFilter}).single('image'))
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
@@ -26,12 +49,11 @@ app.use((req, res, next) => {
 app.use("/feed", feedRoutes);
 
 app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
-    res.status(status).json({message: message});
-})
-
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
--- controllers/feed.js
+++ controllers/feed.js
@@ -24,12 +24,18 @@ exports.createPosts = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+  if(!req.file) {
+    const error = new Error('No images provided');
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path.replace("\\" ,"/");
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: "images/duck.jpg",
+    imageUrl: imageUrl,
     creator: { name: "Emmanuel" },
   });
 
