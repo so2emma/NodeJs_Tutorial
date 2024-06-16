cb0c89cf2b3708b2c9dca79c41b95ef72d95b823
so2emma <osoemmanuel1969@gmail.com>
2024-06-16 23:11:46 +0100

    Signing users up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,35 @@
-const {validationResult} = require('express-validator')
-const User = require('../models/user');
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
 exports.signup = (req, res, next) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        const error = new Error('Validation failed');
-        error.statusCode = 402;
-        error.data = errors.array();
-        throw error
-    }
-    const email = req.body.email;
-    const name = req.body.name;
-    const password = req.body.password;
-
-
-}
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed");
+    error.statusCode = 402;
+    error.data = errors.array();
+    throw error;
+  }
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPw => {
+        const user = new User({
+            email: email,
+            password: hashedPw,
+            name: name
+        });
+        return user.save();
+    })
+    .then(result => {
+        res.status(201).json({message: 'User created', userId: result._id})
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
