623ff36135b970e52b6da1a85c8dc2361e89e31d
so2emma <osoemmanuel1969@gmail.com>
2024-06-16 21:48:41 +0100

    adding user model

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -47,6 +48,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
