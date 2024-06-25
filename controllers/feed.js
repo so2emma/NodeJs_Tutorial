b38675e6aaab5f35519fc9acf1443eee9b7d8674
so2emma <osoemmanuel1969@gmail.com>
2024-06-25 21:13:53 +0100

    clearing posts and user relationship when a post is deleted

--- controllers/feed.js
+++ controllers/feed.js
@@ -186,9 +186,14 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndDelete(postId);
     })
     .then((result) => {
-      console.log(result);
-      res.status(200).json({ message: "Deleted post." });
-    })
+      return User.findById(req.userId);
+    }).then(user =>{
+      user.posts.pull(postId);
+      return user.save();
+    }).then(result => {
+    res.status(200).json({ message: "Deleted post." });
+
+  })
     .catch((err) => {
       if (!err.statusCode) {
         err.statusCode = 500;
