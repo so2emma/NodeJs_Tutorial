a3a4f2ea480b45967d14edadd8fd30fa967fd405
so2emma <osoemmanuel1969@gmail.com>
2024-06-25 11:04:00 +0100

    working on mapping posts to user

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,6 +4,7 @@ const path = require("path");
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
+const User = require("../models/user");
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -62,19 +63,30 @@ exports.createPosts = (req, res, next) => {
   const imageUrl = req.file.path.replace("\\", "/");
   const title = req.body.title;
   const content = req.body.content;
+  let creator;
   const post = new Post({
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: { name: "Emmanuel" },
+    creator: req.userId,
   });
 
   post
     .save()
-    .then((result) => {
+    .then(result => {
+      return User.findById(req.userId);
+    })
+    .then(user => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+      
+    })
+    .then(result => {
       res.status(201).json({
         message: "Post created successfully!",
-        post: result,
+        post: post,
+        creator: {_id: creator._id, name: creator_name}
       });
     })
     .catch((err) => {
