e3b799ecf1530db59fd8a18ced3139db733cdd0f
so2emma <osoemmanuel1969@gmail.com>
2024-06-25 21:06:38 +0100

    adding authorization checks

--- controllers/feed.js
+++ controllers/feed.js
@@ -142,7 +142,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
-
+      if(post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorised!');
+        error.statusCode = 403;
+        throw error;
+      }
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -169,6 +173,11 @@ exports.deletePost = (req, res, next) => {
       if (!post) {
         const error = new Error("Could not find post.");
         error.statusCode = 404;
+        throw error;
+      }
+      if(post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorised!');
+        error.statusCode = 403;
         throw error;
       }
       // this is for checking logged in User
